0f0c7fc947b9a44f1e7fcec1e1823482a6674f27
ntrsai <161465047+ntrsai@users.noreply.github.com>
2024-07-16 16:23:55 +0530

    Delete shopify directory

--- shopify/seller/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from seller import models
-
-# Register your models here.
-admin.site.register(models.Product)
--- shopify/user/user_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from user import views
-
-urlpatterns = [
-    path('', views.home),
-    path('register/', views.register),
-    path('login/', views.user_login),
-    path('logout/', views.user_logout),
-]
